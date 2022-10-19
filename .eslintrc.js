c3acca27d52be6ac9c1fe90b75ffb6d43a901560
roymansoor <roy.mansoor@gmail.com>
2022-10-19 17:10:33 -0500

    program is functional gets both currency as input and give the exchange rate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true
-  },
-  'extends': 'eslint:recommended',
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
-  },
-  'rules': {
-    'indent': [
-      'error',
-      4
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
     ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
-  }
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
 };
--- app.js
+++ app.js
@@ -17,26 +17,21 @@
 
 
 
-
-var fromCurrency = document.getElementsByClassName('fromCurrency')[0].innerHTML;
-var toCurrency = document.getElementsByClassName('toCurrency')[0].innerHTML;
-
-var requestURL = `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}`;
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-    var response = request.response;
-    console.log(response);
-};
-
-   
-
-
 var btnSubmit = document.getElementsByClassName('btnSubmit')[0];
-
 btnSubmit.addEventListener('click', function(){  
-    alert('Hello World!'); 
+    var fromCurrency = document.getElementsByClassName('fromCurrency')[0].value;
+    var toCurrency = document.getElementsByClassName('toCurrency')[0].value;
+ 
+    var requestURL = `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}`;
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    
+    request.onload = function() {
+        var response = request.response;
+        document.getElementsByClassName('output')[0].innerHTML = response.result;
+    };
 });
+
+
