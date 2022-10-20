930aef203e9d4952a054ee0d6b92a31c27e60840
roymansoor <roy.mansoor@gmail.com>
2022-10-19 22:02:03 -0500

    version 2 is working, you can select the curancies from the drop down menu

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
 
-
-//function search(){
-
-// var $input = $('EUR').val()
-// var $input2 =$('USD').val()
-// $.get(`https://api.exchangerate.host/convert?from=${USD}&to=${EUR}`, (data) => {
-//     //console.log(data);
-  
-  
-
-  
-// console.log(data);
-  
-//   });
-//}
-
-
-
+// Phase 1, get both currancies and give back the exchange rate.
 var btnSubmit = document.getElementsByClassName('btnSubmit')[0];
 btnSubmit.addEventListener('click', function(){  
-    var fromCurrency = document.getElementsByClassName('fromCurrency')[0].value;
-    var toCurrency = document.getElementsByClassName('toCurrency')[0].value;
+    var fromCurrency = document.getElementsByClassName('fromCurrency')[0].value.toUpperCase();
+    var toCurrency = document.getElementsByClassName('toCurrency')[0].value.toUpperCase();
  
     var requestURL = `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}`;
     var request = new XMLHttpRequest();
@@ -28,10 +11,66 @@ btnSubmit.addEventListener('click', function(){
     request.responseType = 'json';
     request.send();
     
+
+
     request.onload = function() {
         var response = request.response;
         document.getElementsByClassName('output')[0].innerHTML = response.result;
     };
 });
 
+// Phase 2, user being able to select the currency from dropdown menu.
 
+
+
+// var btnSubmit = document.getElementsByClassName('btnSubmit')[0];
+// btnSubmit.addEventListener('click', function(){  
+//     var firstCurrency = document.getElementById('Currency1').value;
+//     var secondCurrency = document.getElementById('Currency2').value;
+ 
+
+var requestURL = 'https://api.exchangerate.host/symbols';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    var response = request.response;
+    //console.log(response);
+
+    var listOfCurrencies = response.symbols;
+    var options = document.createElement('option');
+    var select1 = document.getElementById('Currency1');
+    var select2 = document.getElementById('Currency2');
+
+    for(var index in listOfCurrencies){
+        //console.log(index);
+        options.innerText = index;
+        select1.appendChild(options.cloneNode(true));
+        select2.appendChild(options.cloneNode(true));
+    }
+
+};
+
+
+function getResult(){
+
+    var btnSubmit2 = document.getElementsByClassName('btnSubmit2')[0];
+    btnSubmit2.addEventListener('click', function(){ 
+
+        var firstCurrency = document.getElementById('Currency1').value;
+        var secondCurrency = document.getElementById('Currency2').value;
+        var requestURL = `https://api.exchangerate.host/convert?from=${firstCurrency}&to=${secondCurrency}`;
+        var request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+        request.onload = function() {
+            var response = request.response;
+            document.getElementsByClassName('output2')[0].innerHTML = response.result;
+        };
+    });
+}
+
+getResult();
